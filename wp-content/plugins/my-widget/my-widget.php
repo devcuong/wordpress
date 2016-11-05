c36df3cb8458bb644b173e4f08f4387e7136a345
cuong1503 <cuong.dh8c@gmail.com>
2016-11-05 16:42:40 +0700

    5/11

---
+++ wp-content/plugins/my-widget/my-widget.php
@@ -0,0 +1,89 @@
+<?php
+/*
+Plugin Name: My Widget
+Plugin URI: http://thachpham.com
+Description: tao widget myself
+Author: Thach Pham
+Version: 1.0
+Author URI: http://google.com
+*/
+/*
+ * Kh?i t?o widget item
+ */
+add_action( 'widgets_init', 'create_thachpham_widget' );
+function create_thachpham_widget() {
+        register_widget('Thachpham_Widget');
+}
+
+/*
+* Tao Widget
+*
+*/
+class Thachpham_Widget extends WP_Widget {
+ 
+        /**
+         * Thit lp widget:
+         */
+        function __construct() {
+			parent::__construct(
+			'thachpham_widget',
+			'ThachPham Widget',
+			
+			array(
+			'description' => 'Mo ta cua widget' // mo ta
+			)
+			
+			);
+        }
+ 
+        /**
+         * To form option cho widget
+         */
+        function form( $instance ) {
+			//Biến tạo các giá trị mặc định trong form
+			$default = array(
+					'title' => 'Ten cua ban'
+			);
+			$instance = wp_parse_args( (array) $instance, $default);
+			$title = esc_attr( $instance['title'] );
+			echo "Nhập tiêu đề <input class='widefat' type='text' name='".$this->get_field_name('title')."' value='".$title."' />";
+        }
+ 
+        /**
+         * save widget form
+         */
+ 
+        function update( $new_instance, $old_instance ) {
+			parent::update( $new_instance, $old_instance );
+			$instance = $old_instance;
+			$instance['title'] = strip_tags($new_instance['title']);
+			return $instance;
+        }
+ 
+        /**
+         * Show widget
+         */
+ 
+        function widget( $args, $instance ) {
+			extract( $args );
+			$title = apply_filters( 'widget_title', $instance['title'] );
+	 
+			echo $before_widget;
+	 
+			//In tiêu đề widget
+			// var_dump($args['before_title']);
+			echo "<div>".$before_title.$title.$after_title."</div>";
+	 
+			// Nội dung trong widget
+			$popularpost = new WP_Query( array( 'post_type'     =>  'News','posts_per_page' => 5, 'meta_key' => 'wpb_post_views_count', 'orderby' => 'meta_value_num', 'order' => 'DESC'  ) );
+			while ( $popularpost->have_posts() ) : $popularpost->the_post();
+			echo "<br>";
+			the_title(); 
+
+			endwhile;
+			// Kết thúc nội dung trong widget
+	 
+			echo $after_widget;
+        }
+ 
+}
--- wp-content/themes/thachpham/single.php
+++ wp-content/themes/thachpham/single.php
@@ -1,7 +1,7 @@
 <?php get_header(); ?>
 <div class="content">
     <div id="main-content">
-        <?php if ( have_posts() ) : while( have_posts() ) : the_post(); ?>
+        <?php if ( have_posts() ) : while( have_posts() ) : the_post(); wpb_set_post_views(get_the_ID()); ?>
         
         <?php get_template_part('content', get_post_format()); ?>
         <?php get_template_part('author-bio'); ?>
