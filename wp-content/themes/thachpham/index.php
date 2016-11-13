93b783870b9ba1b575cbc7d2a8d8de869907a1d4
cuong1503 <cuong.dh8c@gmail.com>
2016-11-13 14:40:05 +0700

    13/11/2016

--- wp-content/themes/thachpham/index.php
+++ wp-content/themes/thachpham/index.php
@@ -3,6 +3,11 @@
 <div class="col-left">
 	<?php get_sidebar('top-news')?>
     <div id="main-content">
+	<div id="ctl40_HeaderContainer" class="title_post">
+          <h2><a><span style="white-space:nowrap;">Tin rao dành cho bạn</span></a></h2>
+    </div>
+	<div style="clear: both;"></div>
+	<div class="line_gr"></div>
         <?php if ( have_posts() ) : while( have_posts() ) : the_post(); ?>
         
         <?php get_template_part('content', get_post_format()) ?>
