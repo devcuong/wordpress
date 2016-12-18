05c9b987b72faef42fecd4078d460d77027c6267
devcuong <cuong.dh8c@gmail.com>
2016-12-17 21:03:59 -0800

    17122016

---
+++ wp-content/themes/thachpham/sidebar-manage-right.php
@@ -0,0 +1,42 @@
+<div class="menu-user">
+    <div class="mu-title">
+        <h3>Thông tin tài khoản</h3>
+    </div>
+	<ul>
+		<li class="lead"><a href="javascript:">Quản lý tin rao</a></li>
+		<li><a href="/thanh-vien/dang-tin-ban-cho-thue-nha-dat.htm">Đăng tin
+				rao bán/cho thuê</a></li>
+		<li class="active"><a href="/thanh-vien/quan-ly-tin.htm">Quản lý tin
+				rao bán/cho thuê</a></li>
+		<li><a href="/thanh-vien/quan-ly-tin-da-luu.htm">Quản lý tin rao đã
+				lưu</a></li>
+		<li><a href="/thanh-vien/quan-ly-tin-nhap.htm">Quản lý tin nháp</a></li>
+		<li><a href="/thanh-vien/quan-ly-nhan-bat-dong-san-qua-email.htm">Quản
+				lý đăng ký nhận BĐS qua email</a></li>
+
+		<div id="ContentPlaceHolder1_BoxMenu1_divBranchUserRequest">
+			<li class="lead"><a href="javascript:">Tài khoản nhánh</a></li>
+			<li><a target="_blank"
+				href="http://file.dothi.net/huong-dan/huong-dan-dang-ky-tinh-nang-tai-khoan-nhanh.html">Hướng
+					dẫn đăng ký Tính năng QL TK nhánh</a></li>
+			<li id="mnUpgradeAcc"><a
+				href="/thanh-vien/yeu-cau-chuc-nang-tai-khoan-nhanh.htm">Yêu cầu
+					tính năng TK nhánh</a></li>
+		</div>
+
+
+		<li class="lead"><a href="javascript:">Thông tin giao dịch</a></li>
+		<li><a href="/thanh-vien/goi-tin.htm">Gói tin</a></li>
+		<li><a href="/thanh-vien/lich-su-giao-dich.htm">Lịch sử giao dịch</a></li>
+		<li id="mnChargeMoney"><a href="/thanh-vien/nap-tien.htm">Nạp tiền</a></li>
+		<li class="lead"><a href="javascript:">Khuyến mại </a></li>
+
+		<li><a href="/thanh-vien/quan-ly-khuyen-mai.htm">Quản lý khuyến mại</a></li>
+
+		<li class="lead"><a href="/bao-gia.htm">Báo giá</a></li>
+		<li class="lead"><a href="/huong-dan.htm">Hướng dẫn sử dụng</a> <!--
+</ul--> <input type="hidden"
+			name="ctl00$ContentPlaceHolder1$BoxMenu1$hdUserType" id="hdUserType"
+			value="0"></li>
+	</ul>
+</div>
